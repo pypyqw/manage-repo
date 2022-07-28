dbdca4dc561024e27f75fe7b6500ef3178952816
Ruud <110191970+pypyqw@users.noreply.github.com>
2022-07-28 20:03:10 +0200

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { Command } from 'commander';
+import * as features from './features';
+
+const program = new Command();
+
+Object.keys(features)
+    .forEach(feature => {
+        const fn = features[feature];
+        if ('function' === typeof fn) {
+            fn(program);
+        }
+    });
+
+    program.description(require('../package.json').description)
+    program.description('ololo')
+    program.version(require('../package.json').version)
+    program.parse(process.argv);
